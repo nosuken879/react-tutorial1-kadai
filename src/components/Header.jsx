b1ac15b813fddcc1e794ca297b13f5ab25ced749
nosuken879 <chyn20002@g.nihon-u.ac.jp>
2024-12-29 02:50:16 +0900

    新規作成

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+/* ヘッダー */
+import Nav from "./Nav";
+
+export default function Header() {
+  return(
+    <header>
+      <h1>Birdwatching</h1>
+      <div className = "header img">
+        <img src="dove.png" alt="a simple dove logo"></img>
+      </div>
+      <Nav />
+    </header>
+  );
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+/* メイン */
+import Article from "./Article";
+import Aside from "./Aside";
+
+export default function Main() {
+  return(
+    <main>
+      <h2>Welcome</h2>
+      <Article/>
+      <Aside/>
+    </main>
+
+  );
+}
